9396c9471f0ad405600fcc1201a7c97a49a0b767
Ruthvik <Ruthvik4707@gmail.com>
2020-12-23 02:24:57 -0600

    r :wq

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <header id="home">
+                <nav>
+
+                </nav>
+
+            </header>
+         );
+    }
+}
+ 
+export default Footer;
