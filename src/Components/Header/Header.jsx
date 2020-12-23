ea8aa34e3379ec078af506e1e0dde594aa0dc847
Ruthvik <Ruthvik4707@gmail.com>
2020-12-23 02:29:05 -0600

    second_try

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -15,36 +15,7 @@ class Header extends Component {
         }
 
         return ( 
-            <header id="home">
-
-            <nav id="nav-wrap">
-
-               <ul id="nav" className="nav">
-                  <li className="current"><a  href="#home">Home</a></li>
-                  <li><a href="#about">About</a></li>
-                  <li><a href="#resume">Resume</a></li>
-                  <li><a href="#portfolio">Works</a></li>
-                  <li><a href="#contact">Contact</a></li>
-               </ul>
-
-            </nav>
-
-            <div className="row banner">
-               <div className="banner-text">
-                  <h1 className="responsive-headline">I'm {myname}.</h1>
-                  <h3>I'm a US based <span>{occupation}</span>. {description}.</h3>
-                  <hr />
-                  <ul className="social">
-                     {networks}
-                  </ul>
-               </div>
-            </div>
-
-            <p className="scrolldown">
-               <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-            </p>
-
-         </header>
+       
            
        
          );
